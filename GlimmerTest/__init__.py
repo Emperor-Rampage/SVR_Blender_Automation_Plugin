24a82d570e83835ce287648ac27a41a8d4239fbe
Conner <connerlindsley@gmail.com>
2021-01-29 17:05:55 -0800

    Update __init__.py

--- GlimmerTest/__init__.py
+++ GlimmerTest/__init__.py
@@ -155,16 +155,16 @@ classes = (
 
 def register():
 
-    #py_exec = bpy.app.binary_path_python
+    py_exec = bpy.app.binary_path_python
 
     # ensure pip is installed & update
-    #subprocess.call([str(py_exec), "-m", "ensurepip", "--user"])
-    #subprocess.call([str(py_exec), "-m", "pip", "install", "--upgrade", "pip"])
+    subprocess.call([str(py_exec), "-m", "ensurepip", "--user"])
+    subprocess.call([str(py_exec), "-m", "pip", "install", "--upgrade", "pip"])
 
     # install dependencies using pip
     # dependencies such as 'numpy' could be added to the end of this command's list
-    #subprocess.call([str(py_exec),"-m", "pip", "install", "--user", "imageio"])
-    #subprocess.call([str(py_exec),"-m", "pip", "install", "--user", "moviepy"])
+    subprocess.call([str(py_exec),"-m", "pip", "install", "--user", "imageio"])
+    subprocess.call([str(py_exec),"-m", "pip", "install", "--user", "moviepy"])
 
     for cls in classes:
         bpy.utils.register_class(cls)
